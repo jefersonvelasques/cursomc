5bb2682da061d6f635bf8609cace9e5b941b3d00
jeferson velasques <jeferson.rodrigues@ifms.edu.br>
2018-10-06 22:21:02 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/edu/ifms/cursomc/resources/CategoriaResources.java
+++ src/main/java/edu/ifms/cursomc/resources/CategoriaResources.java
@@ -1,16 +1,28 @@
 package edu.ifms.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.ifms.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "O REST está funcionando !";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"informática");
+		Categoria cat2 = new Categoria(2,"escritório");
+		
+		List <Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
