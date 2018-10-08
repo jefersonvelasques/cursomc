d21df248a1fc3f9a9e655300a9481a915803fc86
jeferson velasques <jeferson.rodrigues@ifms.edu.br>
2018-10-07 22:28:06 -0300

    Instanciando dados no banco

--- src/main/java/edu/ifms/cursomc/CursomcApplication.java
+++ src/main/java/edu/ifms/cursomc/CursomcApplication.java
@@ -1,12 +1,32 @@
 package edu.ifms.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import edu.ifms.cursomc.domain.Categoria;
+import edu.ifms.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+    @Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"informática");
+		Categoria cat2 = new Categoria(null,"escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		
+		
+		
 	}
 }
